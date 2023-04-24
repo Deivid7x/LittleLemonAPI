1ad38e78e501246d285e98c4737c2531a21c062d
Deivid7x <115668940+Deivid7x@users.noreply.github.com>
2023-04-24 11:32:35 -0300

    creating the OrderItem model

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -34,3 +34,12 @@ class Order(models.Model):
     date = models.DateField(db_index=True)
 
 
+class OrderItem(models.model):
+    order = models.ForeignKey(User, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('order', 'menuitem')
